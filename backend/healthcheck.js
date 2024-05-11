cd0fd978138415eac6e43919a3b345a0bec1e20f
Bhagwan Komalram <bhagwan.ram.komalram@gmail.com>
2024-05-10 21:48:18 -0500

    adecuando healthcheck

--- backend/healthcheck.js
+++ backend/healthcheck.js
@@ -4,7 +4,7 @@ const options = {
   timeout: 2000,
   host: "localhost",
   port: process.env.PORT || 8080,
-  path: "/healthz" // must be the same as HEALTHCHECK in Dockerfile
+  path: "/healthz", // must be the same as HEALTHCHECK in Dockerfile
   headers: {
     'api-key': 'a2a47f86-c361-4fed-98ec-6b36eeef0266'
   }
