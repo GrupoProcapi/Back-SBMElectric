f2a5a78f708c86e980a30d1bd960fac549f7588c
Bhagwan Komalram <bhagwan.ram.komalram@gmail.com>
2024-05-19 12:37:11 -0500

    agregando query de cuentas en el QB v1

--- backend/src/SOAPserver.js
+++ backend/src/SOAPserver.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 4747;
 
 app.use(bodyParser.raw({ type: 'text/xml' }));
+const tempFSRead = path.join(__dirname, 'AccountQuery.xml');
+
+const AccountQuery = fs.readFileSync(tempFSRead, 'utf8');
 
 const service = {
   QBWebConnectorSvc: {
@@ -20,15 +25,19 @@ const service = {
       },
       authenticate: (args, callback) => {
         console.log('authenticate called');
-        const ticket = 'your-ticket-string';
+        const ticket = 'SBM-QBWC-MEASURER-CONSUME-0001';
         callback(null, { authenticateResult: { 'string': [ticket, ''] } });
       },
       sendRequestXML: (args, callback) => {
+        console.log("Argumentos que envia el QBWC")
+        console.log(args)
         console.log('sendRequestXML called');
-        const requestXML = '<QBXML></QBXML>'; // Tu XML aquí
+        const requestXML = AccountQuery;
         callback(null, { sendRequestXMLResult: requestXML });
       },
       receiveResponseXML: (args, callback) => {
+        console.log("Argumentos que envia el QBWC en la respuesta")
+        console.log(args)
         console.log('receiveResponseXML called');
         const response = 100; // Percent done
         callback(null, { receiveResponseXMLResult: response });
