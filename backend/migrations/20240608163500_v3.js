b5fb408c489a0520f03ec691740a8efa90bcc161
Bhagwan Komalram <bhagwan.ram.komalram@gmail.com>
2024-06-08 16:38:19 -0500

    agregando columna de muelle para los medidores measueres v.2

--- backend/migrations/20240608163500_v3.js
+++ backend/migrations/20240608163500_v3.js
@@ -1,11 +1,5 @@
 exports.up = function(knex) {
     return knex.schema
-    .table('measurements', function (table) {
-      table.string('sbmqb_customer_name', 500).nullable().alter();
-      table.string('description', 300).notNullable().alter();
-      table.decimal('total_measure_value');
-      table.integer('sbmqb_invoices_id');
-    })
     .table('measurers', function (table) {
       table.string('muelle', 100);
     })
