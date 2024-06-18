abfcb9e99dc2a54e7a96b9cdd9145a9a0e58f55f
Bhagwan Komalram <bhagwan.ram.komalram@gmail.com>
2024-06-17 21:50:29 -0500

    Validando api-key v2

--- backend/src/apiKeyValidator.js
+++ backend/src/apiKeyValidator.js
@@ -1,7 +1,7 @@
 const config = require('./config');
 
 const apiKeyValidator = (req, res, next) => {
-  const apiKey = req.headers['api-key'];
+  const apiKey = req.headers['Api-Key'];
 
   if (!apiKey) {
     return res.status(401).json({ error: 'API key is missing' });
