aedb674e65ac2621bc552b1b21f1332f4bcc4a55
Bhagwan Komalram <bhagwan.ram.komalram@gmail.com>
2024-06-17 20:24:51 -0500

    Medidas iniciales y servicios asociados a usuarios v1

--- backend/seeds/sbmqb_customers.js
+++ backend/seeds/sbmqb_customers.js
@@ -1,25 +1,9 @@
 exports.seed = function(knex) {
     // Deletes ALL existing entries
+    /*
     return knex('sbmqb_customers').del()
       .then(function () {
         // Inserts seed entries
-        return knex('sbmqb_customers').insert([
-          {
-            sbmqb_id: "80001286-1639401114",
-            name: "2 FAST 4 YOU, Vienna Cat LTD.",
-            full_name: "2 FAST 4 YOU, Vienna Cat LTD.",
-            company_name: "2 FAST 4 YOU, Vienna Cat LTD.",
-            class: "MARINA",
-            status: "ACTIVE"
-          },
-          {
-            sbmqb_id: "80001286-16394",
-            name: "Test BK - 1",
-            full_name: "Test BK - 1",
-            company_name: "Test BK - 1",
-            class: "MARINA",
-            status: "ACTIVE"
-          }
-        ]);
-      });
+        return knex('sbmqb_customers').insert([]);
+      });*/
   };
--- backend/seeds/users.js
+++ backend/seeds/users.js
@@ -3,11 +3,11 @@ exports.seed = function(knex) {
     return knex('users').del()
       .then(function () {
         // Inserts seed entries
-        return knex('users').insert([
+        /*return knex('users').insert([
           {username:'diaz', password: btoa('testPassword.'), role: 'FACTURADOR'},
           {username:'bravo', password: btoa('testPassword'), role: 'OPERADOR'},
           {username:'alpha', password: btoa('testPassword'), role: 'ADMINISTRADOR'},
           {username:'testUser', password: btoa('testPassword'), role: 'ADMINISTRADOR'},
-        ]);
+        ]);*/
       });
   };
